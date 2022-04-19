4f9608dedd744325f95b1ba0e3e45c46835932e0
FawnTizer <100979291+FawnTizer@users.noreply.github.com>
2022-04-19 14:49:35 -0500

    Add files via upload

---
+++ js/Bala.js
@@ -0,0 +1,24 @@
+class Bala {
+    constructor(x, y ) {
+      this.x = x;
+      this.y = y;
+     var options = {
+         isStatic = true
+
+     };
+      this.r=30;
+      this.body = Bodies.circle(x,y,this.r,options):
+      this.image=loadImage ("./assets/cannonball.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.options;
+      push();
+    imageMode(CENTER);
+    image(this.image,pos.x,pos.y,this.r,this.r);
+    pop();
+    
+    
+    }
+  }
+  
